bfeb7089e0212874ac3be918559c9ff973dc478b
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-12-26 16:31:37 -0500

    [view-config] Add missing files

---
+++ src/app/components/wrapper-components/local-game-configuration/local-game-configuration.component.ts
@@ -0,0 +1,79 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+import { MGPOptional } from '@everyboard/lib';
+
+import { BaseWrapperComponent } from '../BaseWrapperComponent';
+import { DemoNodeInfo } from '../demo-card-wrapper/demo-card-wrapper.component';
+import { ConfigDescriptionType, RulesConfig } from 'src/app/jscaip/RulesConfigUtil';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GameState } from 'src/app/jscaip/state/GameState';
+import { AbstractNode, GameNode } from 'src/app/jscaip/AI/GameNode';
+
+
+/**
+ * This component appears when we start a local game.
+ * Its role is to setup the configuration, possibly letting the user choose the config if the game allows it.
+ * When the config is selected, we switch to LocalGameWrapper.
+ * If the game is not configurable, we directly switch to LocalGameWrapper.
+ */
+@Component({
+    selector: 'app-local-game-configuration',
+    templateUrl: './local-game-configuration.component.html',
+})
+export class LocalGameConfigurationComponent extends BaseWrapperComponent {
+
+    public configDemo: DemoNodeInfo;
+
+    public rulesConfig: MGPOptional<RulesConfig> = MGPOptional.empty();
+
+    public constructor(activatedRoute: ActivatedRoute,
+                       private readonly router: Router,
+                       private readonly cdr: ChangeDetectorRef)
+    {
+        super(activatedRoute);
+    }
+
+    private setConfigDemo(config: RulesConfig): void {
+        const stateProvider: MGPOptional<(config: MGPOptional<RulesConfig>) => GameState> = this.getStateProvider();
+        if (stateProvider.isPresent()) {
+            const node: AbstractNode = new GameNode(stateProvider.get()(MGPOptional.of(config)));
+            this.configDemo = {
+                click: MGPOptional.empty(),
+                name: this.getGameUrlName(),
+                node,
+            };
+            this.cdr.detectChanges();
+        }
+    }
+
+    public getConfigDemo(): DemoNodeInfo {
+        return this.configDemo;
+    }
+
+    public async updateConfig(rulesConfig: MGPOptional<RulesConfig>): Promise<void> {
+        this.rulesConfig = rulesConfig;
+        // If there is no config for this game, then rulesConfig value will be MGPOptional.empty()
+        if (rulesConfig.isPresent()) {
+            this.setConfigDemo(rulesConfig.get());
+            if (Object.keys(rulesConfig.get()).length === 0) {
+                // There is nothing to configure for this game!
+                await this.startGame();
+            }
+        }
+    }
+
+    public async startGame(): Promise<void> {
+        if (this.rulesConfig.isPresent()) {
+            const rulesConfig: RulesConfig = this.rulesConfig.get();
+            const queryParams: { [key: string]: string } =
+                Object.fromEntries(Object.entries(rulesConfig)
+                    .map((configElement: [string, ConfigDescriptionType]) => {
+                        return [configElement[0], JSON.stringify(configElement[1])];
+                    }));
+            await this.router.navigate(['/local', this.getGameUrlName()], { queryParams: queryParams });
+        } else {
+            await this.router.navigate(['/local', this.getGameUrlName()]);
+        }
+    }
+
+}
